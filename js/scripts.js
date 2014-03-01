23a1dbaa073782eb4edf4bad7da513d05c671603
Mark Lehman & Nolan Miller <student@epicodus.com>
2014-02-28 16:20:26 -0800

    fixes table and adds glorious bg and imgs

--- js/scripts.js
+++ js/scripts.js
@@ -51,16 +51,16 @@ var Board = {
             inArow++;
           }          
         });
-      if (inArow === 3) {
+        if (inArow === 3) {
           winner = true;
-      };
-    });  
-  });
+        };
+      });  
+    });
     return winner;
   }
 };
 var Game = {
-  players : [],
+  players: [],
   create: function() {
     var game = Object.create(Game);
     var playerX = Player.create("PlayerX");
@@ -71,10 +71,10 @@ var Game = {
   },
   nextTurn: function(currentPlayer) {
     if (currentPlayer !== Game.players[0].playerName) {
-        return currentPlayer = Game.players[0].playerName;
-      } else if (currentPlayer !== Game.players[1].playerName){
-        return currentPlayer = Game.players[1].playerName;
-      }
+      return currentPlayer = Game.players[0].playerName;
+    } else if (currentPlayer !== Game.players[1].playerName){
+      return currentPlayer = Game.players[1].playerName;
+    }
   }
 };
 
@@ -86,20 +86,20 @@ $(document).ready(function() {
   var currentSpace;
   var winner = false;
 
-  // $('#gameboard td').click(function() {
+  // $('#gameboard div.cell').click(function() {
     
   board.spaces.forEach(function(space) {
-    $('td[data-coordinate="' + '[' + space.coordinateX + ',' + space.coordinateY + ']' + '"]').click(function() {
+    $('div.cell[data-coordinate="' + '[' + space.coordinateX + ',' + space.coordinateY + ']' + '"]').click(function() {
       if (turnCounter === 0 || winner === true) {
         alert("Refresh to start a new game.");      
       } else {
         currentSpace = board.find(space.coordinateX,space.coordinateY);
         if (currentSpace.markedBy === "") {
           if (turnCounter % 2 === 1) {
-            $(this).text("X");
+            $(this).html("<img src='http://realfood.tesco.com/Media/images/Goodness-Icon-4-resize-70508466-33cd-4efb-b7c9-e9880e1b77e9-0-100x120.jpg'>");
             Game.players[0].spotsUsed.push([space.coordinateX,space.coordinateY]);
           } else if (turnCounter % 2 === 0){
-            $(this).text("O");
+            $(this).html("<img src='http://www.knowbrainer.com/forums/forum/i/authorsicons/35938/20080224%20-%204900%20-%20Self%20portrait%20(100%20x%20120px).jpg'>");
             Game.players[1].spotsUsed.push([space.coordinateX,space.coordinateY])
           } 
           currentSpace.markBy(currentPlayer);
